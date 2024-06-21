189518908924814f89587e1e32337cdc5f4dfba6
nitin-dotcom <ni3shirsath@gmail.com>
2024-06-21 20:48:38 +0530

    Java Project Commited By Nitin

---
+++ gitDemo/src/gitDemo/Test.java
@@ -0,0 +1,30 @@
+package gitDemo;
+
+public class Test 
+{
+	public void m1()
+	{
+		System.out.println("This is M1 Method"); 
+	}
+	public void m2()
+	{
+		System.out.println("This is M2 Method");
+	}
+	public void m3()
+	{
+		System.out.println("This is M3 Method");
+	}
+	
+	public static void main(String[] args) 
+	{
+	  Test t=new Test();
+		
+	  t.m1();
+	  
+	  t.m2();
+	  
+	  t.m3();
+	  
+	}
+
+}
